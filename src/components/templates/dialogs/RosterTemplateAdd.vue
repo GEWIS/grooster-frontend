eef3de28c7ee61387e721690232f4d1544cd7be0
Sven Mokveld <svenmokveldje@gmail.com>
2025-09-11 15:58:49 +0200

    feat: roster templates

--- src/components/templates/dialogs/RosterTemplateAdd.vue
+++ src/components/templates/dialogs/RosterTemplateAdd.vue
@@ -1,9 +1,100 @@
 <script setup lang="ts">
+import {computed, reactive, ref} from "vue";
+import ApiService from "@/services/ApiService";
+import {RosterTemplateCreateRequest} from "@gewis/grooster-backend-ts";
+import {useRoute} from "vue-router";
+import {useTemplateStore} from "@/stores/template.store";
+
+const route = useRoute();
+const  templateStore = useTemplateStore();
+
+const props = withDefaults(
+    defineProps<{
+        open: boolean;
+    }>(),
+    {
+        open: false,
+    }
+)
+
+const emit = defineEmits(["close"]);
+
+const visible = computed({
+    get: () => props.open,
+    set: () => {
+        emit('close');
+    },
+});
+
+const initialValues = reactive({
+    shiftName: '',
+});
+const shifts = ref<string[]>([]);
+
+const resolver = ({ values }) => {
+    const errors = { shiftName: [] };
+
+    if (!values.shiftName) {
+        errors.shiftName.push({ message: 'A shift name is required.' });
+    }
+
+    return {
+        values,
+        errors
+    };
+};
+
+const onFormSubmit = (e) => {
+    if (e.valid) {
+        shifts.value.push(e.values.shiftName);
+    }
+}
+
+const removeShift = (index: number) => {
+    shifts.value.splice(index, 1);
+}
+
+const saveTemplate = async () => {
+    const params: RosterTemplateCreateRequest = {
+        name: 'Test',
+        organId: parseInt(route.params.id as string),
+        shifts: shifts.value,
+    }
+    await templateStore.createTemplate(params);
+
+    shifts.value = [];
+    emit('close');
+}
 
 </script>
 
 <template>
-  
+    <Dialog
+        v-model:visible="visible"
+        modal
+        :style="{ width: '25rem' }"
+        @hide="emit('close')"
+    >
+        <template #header>
+            <div class="w-full text-center">
+                <span class="font- text-lg">Add Roster Template</span>
+            </div>
+        </template>
+        <div>
+            <Form v-slot="$form" :resolver :initial-values @submit="onFormSubmit">
+                <div class="flex flex-col gap-1">
+                    <InputText name="shiftName" type="text" placeholder="ShiftName" />
+                    <Message v-if="$form.shiftName?.invalid" severity="error" size="small" variant="simple">{{ $form.shiftName.error?.message }}</Message>
+                </div>
+                <Button type="submit" severity="secondary" label="Add roster" />
+                <Button @click="saveTemplate" severity="success" label="Save Template"/>
+            </Form>
+        </div>
+        <div v-for="(shift, index) in shifts">
+            {{index}}: {{shift}}
+            <Button icon="pi pi-times" @click="removeShift(index)" style="font-size: 1rem"/>
+        </div>
+    </Dialog>
 </template>
 
 <style scoped>
--- src/views/RosterTemplateView.vue
+++ src/views/RosterTemplateView.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
+import RosterTemplate from "@/components/templates/RosterTemplate.vue";
+import {useOrganStore} from "@/stores/organ.store";
+import {computed, onMounted, ref} from "vue";
+import RosterTemplateAdd from "@/components/templates/dialogs/RosterTemplateAdd.vue";
+import {useTemplateStore} from "@/stores/template.store";
+
+const organStore = useOrganStore();
+const templateStore = useTemplateStore();
+
+const templates = computed(() => Object.values(templateStore.templates));
+const addDialog = ref(false);
+
+onMounted(async () => {
+    await templateStore.fetchTemplates(organStore.organ);
+});
 
 </script>
 
 <template>
+    <div class="flex flex-col gap-2">
+        <Card>
+            <template #content>
+                <div class="flex flex-row gap-2">
+                    <div class="font-bold flex items-center">Shift Templates</div>
 
+                    <Button label="Create Template" @click="addDialog=true"/>
+                </div>
+            </template>
+        </Card>
+        <div v-if="templates.length > 0" v-for="template in templates" :key="template.id">
+            <RosterTemplate
+                :template="template"
+            />
+        </div>
+    </div>
+    <RosterTemplateAdd :open="addDialog" @close="addDialog=false"/>
 </template>
 
 <style scoped>
 
-</style>
+</style>4
