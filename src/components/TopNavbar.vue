2d62f76d8c1a55b5f492631fa85ace490148d383
Sven Mokveld <svenmokveldje@gmail.com>
2025-09-12 15:35:22 +0200

    feat:roster templates
    Adds roster templates to the frontend. you can now make a template with
    a name and a n amount of shifts. Next to that you can delete them, the
    actual making of rosters with the template is not possible yet due to
    this not being implemented in the backend.

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -20,6 +20,14 @@ const isActive = (path) => route.path === path;
             </RouterLink>
             <RouterLink
                 v-if="route.path !== '/' && store.organ"
+                :to="`/rosters/${store.organ}`"
+                class="px-4 py-2 rounded-lg hover:bg-gray-400"
+                :class="{ 'bg-gray-400': isActive('/rosters/' + store.organ) }"
+            >
+                Rosters
+            </RouterLink>
+            <RouterLink
+                v-if="route.path !== '/' && store.organ"
                 :to="`/templates/${store.organ}`"
                 class="px-4 py-2 rounded-lg hover:bg-gray-400"
                 :class="{ 'bg-gray-400': isActive('/templates/' + store.organ) }"
