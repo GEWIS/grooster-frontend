1c2c01a8a5db630651b55e8c40adc49e985f555e
Sven Mokveld <svenmokveldje@gmail.com>
2026-01-23 19:46:07 +0100

    chore: remove debug ui

--- src/components/roster/RosterAssignment.vue
+++ src/components/roster/RosterAssignment.vue
@@ -136,20 +136,6 @@ const availableUsersForShift = (shift: SavedShift) => {
       </div>
     </div>
   </div>
-  <div v-else class="empty-state">
-    <p>No shifts found for this roster or loading...</p>
-    <pre style="font-size: 10px">Debug: {{ savedRoster }}</pre>
-  </div>
-  <div style="border: 2px solid red; padding: 10px; background: white; color: black">
-    <strong>ID Type Check:</strong>
-    <ul>
-      <li>Prop ID: {{ props.id }} (Type: {{ typeof props.id }})</li>
-      <li>Store Keys: {{ Object.keys(rosterStore.savedRoster) }}</li>
-      <li>Store Values: {{ rosterStore.savedRoster }}</li>
-      <li>Key Match Found? {{ Object.keys(rosterStore.savedRoster).includes(String(props.id)) }}</li>
-      <li>Raw Data: {{ !!rosterStore.getSavedRoster(props.id) ? 'Found' : 'Not Found' }}</li>
-    </ul>
-  </div>
 </template>
 
 <style scoped></style>
